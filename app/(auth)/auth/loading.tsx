41d87cda30c2dea74307924c8921cf7646ec9701
Hr-kutsarov <hr.kutsarov89@gmail.com>
2024-01-22 00:00:13 +0200

    Update: fallback pages while loading

---
+++ app/(auth)/auth/loading.tsx
@@ -0,0 +1,11 @@
+import LoadingPage from '@/components/loading/loading-page'
+
+function Loading() {
+    return (
+        <main className='bg-zinc-900'>
+        <LoadingPage />
+        </main>
+    )
+}
+
+export default Loading;
